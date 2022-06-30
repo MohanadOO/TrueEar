7fdec2d336ab28f169376c6cb70b01271b906a6a
MohanadOO <moohand220@gmail.com>
2022-07-01 00:19:26 +0300

    Move AuthProvider and ApolloProvider to main.tsx 📁▶

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,43 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from '../src/context/AuthContext'
+
+import { setContext } from '@apollo/client/link/context'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:1337/graphql',
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
