4807f4560ced387b8d8c3c1017bb0631de3a0fc1
MohanadOO <moohand220@gmail.com>
2022-08-12 04:00:28 +0300

    Add HandleAccess.tsx ➕

---
+++ client/src/routes/HandleAccess.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
+import { Navigate, useSearchParams } from 'react-router-dom'
+import { useAuth } from '../context/AuthContext'
+
+function HandleAccess({ provider }: { provider: String }) {
+  const [isLoading, setIsLoading] = useState(true)
+  const [success, setSuccess] = useState<undefined | boolean>(undefined)
+
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const access_token = searchParams.get('access_token')
+  const backendURL = `http://localhost:1330/api/auth/${provider}/callback`
+
+  const { setCurrentUser } = useAuth()
+
+  useEffect(() => {
+    const requestResponse = async () => {
+      const response = await fetch(
+        `${backendURL}?access_token=${access_token}`,
+        {
+          credentials: 'include',
+        }
+      )
+      const data = await response.json()
+      if (data?.error) {
+        toast.error(
+          <>
+            <b>{data.error.status}</b>
+            <span className='ml-2'>{data.error.message}</span>
+          </>
+        )
+        setIsLoading(false)
+        setSuccess(false)
+      } else {
+        const token = data.jwt
+        const user = data.user
+        console.log(user)
+        localStorage.setItem('token', token)
+        setCurrentUser(user)
+        setIsLoading(false)
+        setSuccess(true)
+        toast.success(`Welcome Back ${user.username} 👋`)
+      }
+      try {
+        console.log('Success Response')
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    requestResponse()
+  }, [])
+
+  return (
+    <>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {success ? (
+            <Navigate to='/store' replace />
+          ) : (
+            <Navigate to='/login' replace />
+          )}
+        </>
+      )}
+    </>
+  )
+}
+
+export default HandleAccess
