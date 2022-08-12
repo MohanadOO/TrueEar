eb7cb997035f173efaf7536258581652b9696770
MohanadOO <moohand220@gmail.com>
2022-08-12 03:56:06 +0300

    Delete Backend Folder 🗂❌

--- backend/config/server.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1330),
-  url: "https://4d9a-5-156-54-207.eu.ngrok.io",
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
-});
