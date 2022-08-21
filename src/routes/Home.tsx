d7e7cc5a31899cf3b182b53fed032d520019d30d
MohanadOO <moohand220@gmail.com>
2022-08-21 21:25:56 +0300

    Update Home Page Style 🖌

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -5,33 +5,42 @@ function Home() {
   const { currentUser } = useAuth()
 
   return (
-    <section className='pb-4 px-4 sm:px-8'>
-      <div className='text-center space-y-4'>
-        <h1 className=' font-bold text-4xl md:text-5xl'>
-          The Best
-          <span className='text-primary'> True Wireless Earbuds </span>
-          start with us
-        </h1>
-        <p className=' max-w-xl mx-auto leading-relaxed'>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio
-          aperiam commodi accusamus dolor reiciendis obcaecati.
-        </p>
-      </div>
-      <div className='mt-12 justify-center items-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex'>
-        <Link
-          to='/store'
-          className='px-10 py-3.5 w-full btn btn-primary sm:w-auto'
-        >
-          Store Page
-        </Link>
-        {!currentUser && (
+    <section className='pb-4 px-4 sm:px-8 flex flex-col lg:flex-row justify-between items-center self-center mt-36 lg:mt-0 lg:gap-24'>
+      <div className='lg:max-w-xl'>
+        <div className='text-center lg:text-left space-y-4'>
+          <h1 className='font-bold text-4xl md:text-5xl lg:text-6xl'>
+            Only
+            <span className='text-primary'> The Best </span>
+            True wireless earbuds
+          </h1>
+          <p className='max-w-lg mx-auto lg:text-lg lg:mx-0 leading-relaxed text-primary font-semibold'>
+            Get Rid of annoying Wires and Embrace the feel of premium True
+            Wireless Earbuds
+          </p>
+        </div>
+        <div className='mt-12 justify-center items-center lg:justify-start space-y-3 sm:space-x-6 sm:space-y-0 sm:flex'>
           <Link
-            to='/signup'
-            className='px-10 py-3.5 w-full btn btn-outline sm:w-auto'
+            to='/store'
+            className='px-10 py-3.5 w-full btn btn-primary sm:w-auto'
           >
-            Sign Up
+            Store Page
           </Link>
-        )}
+          {!currentUser && (
+            <Link
+              to='/signup'
+              className='px-10 py-3.5 w-full btn btn-outline sm:w-auto'
+            >
+              Sign Up
+            </Link>
+          )}
+        </div>
+      </div>
+      <div className='max-w-sm w-full my-24'>
+        <img
+          className='aspect-square object-cover bg-primary/40 max-w-sm w-full mask mask-hexagon'
+          src='/home.png'
+          alt='Home Illustration'
+        />
       </div>
     </section>
   )
